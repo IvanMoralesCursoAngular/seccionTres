712cf1f058aaadd1c3161a43e11beee56fb08777
Ivan Morales <jesusmles_11@hotmail.com>
2025-01-15 13:22:54 -0700

    Tipos basicos y conceptos generales

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+// import './topics/01-basic-types'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  Hola mundo
+`
+
+console.log('hola mundo');
+
---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,10 @@
+
+const name = 'Strider';
+let tipo: number | string = 95;
+const bool: boolean = true;
+
+tipo = "asd";
+
+console.log({
+    name, tipo, bool
+});
