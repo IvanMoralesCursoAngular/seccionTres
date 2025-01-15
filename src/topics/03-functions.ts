3d98fe157317d4aa1a993e595b69f562270c316f
Ivan Morales Angular <ivan.morales.curso.angular@hotmail.com>
2025-01-15 16:47:32 -0700

    funciones con objetos como argumentos

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -15,8 +15,35 @@ function multiply( firstNumber:number, secondNumber?:number, base:number = 2 ) {
     return firstNumber * base;
 }
 
-const result:string = addNumbers(1, 2).toString();
-const result2:string = addNumbersArrow(1,2);
-const result3:number = multiply(5);
+// const result:string = addNumbers(1, 2).toString();
+// const result2:string = addNumbersArrow(1,2);
+// const result3:number = multiply(5);
 
-console.log({result, result2, result3});
+//De aqui en adelante es otro video
+
+//Cree una interfaz con un metodo como parametro
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+
+//Funcion  para aumentar p
+const healCharacter = ( character: Character, amount:number ) => {
+    character.hp += amount;
+}
+
+//Cree el objeto de tipo character
+const strider: Character = {
+    name: 'strider',
+    hp: 50,
+    showHp() {
+        console.log(`Puntos de vida ${ this.hp }`);
+    }
+}
+
+
+healCharacter(strider, 10);
+healCharacter(strider, 30);
+
+strider.showHp();
