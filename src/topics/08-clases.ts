5ffb3d983048024f99b877307ed86406e265a8ff
Ivan Morales Angular <ivan.morales.curso.angular@hotmail.com>
2025-01-17 14:31:05 -0700

    extender una clase

--- src/topics/08-clases.ts
+++ src/topics/08-clases.ts
@@ -15,8 +15,26 @@ export class Person {
     
 }
 
+//Cree la clase Hero para que aplique herencia de Person, super( los que vienen de person los pongo aqui, los tengo que agregar al constructor)
+export class Hero extends Person {
+    
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+        // address: string
+    ) {
+        // super(realName, address);
+        super(realName, 'San Jose, CA.');
+    }
+}
+
+
+
+
 //aqui creo una instancia de persona llamada ironman (Que se note que esta fuera del scope de la clase Person)
-const ironman = new Person('Ivan', 'San Jose, CA.');
+// const ironman = new Hero('ironMan', 45, 'Tony', 'San Jose, CA.');
+const ironman = new Hero('ironMan', 45, 'Tony');
 
 //Aqui si pongo ironman.address, si me lo permite pero no me aparece 
 //y si lo puedo poner y si se va imprimir, pero no esta bien que quede asi
